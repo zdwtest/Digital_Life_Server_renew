c40e1f953b3bcdde9fdb9ba8e6055d261b129c37
ZHANG DAWEI <729913310@qq.com>
2024-11-19 00:22:53 +0800

    Update run-api-websocket.bat

--- run-api-websocket.bat
+++ run-api-websocket.bat
@@ -1,6 +1,6 @@
 set SCRIPT_NAME=SocketServer.py
 set CHATVER=3
-set APIKey=sk-0c8c295cb63e4cf2b1c46362cd23adf6
+set APIKey=sk-xxxx
 set PROXY=http://127.0.0.1:7890
 #set baseUrl=https://dashscope.aliyuncs.com/compatible-mode/v1
 set STREAM=False
@@ -8,4 +8,4 @@ set CHARACTER=yunfei
 set MODEL=gpt-3.5-turbo
 
 
-.\venv\Scripts\python.exe WebsocketServer.py  --baseUrl "https://dashscope.aliyuncs.com/compatible-mode/v1" --chatVer 3 --APIKey sk-0c8c295cb63e4cf2b1c46362cd23adf6 --stream False --character yunfei --model qwen-max
+.\venv\Scripts\python.exe WebsocketServer.py  --baseUrl "https://dashscope.aliyuncs.com/compatible-mode/v1" --chatVer 3 --APIKey sk-xxxx --stream False --character yunfei --model qwen-max
